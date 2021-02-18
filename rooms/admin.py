8cb7849288888a91a508ebeff2e4054a7c0c0cb0
bunnycast <berzzubunny@gmail.com>
2021-02-18 12:07:46 +0900

    expand rooms model 2 1. Verbose Name Plural with Meta class 2. add Photo models

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,13 +1,23 @@
 from django.contrib import admin
 
-from rooms.models import Room, RoomType, Amenity, Facility, HouseRule
+from rooms.models import Amenity, Facility, HouseRule, RoomType, Room, Photo
 
 
-@admin.register(Amenity, Facility, HouseRule, RoomType, Room)
-class RoomAdmin(admin.ModelAdmin):
+@admin.register(Amenity, Facility, HouseRule, RoomType)
+class ItemAdmin(admin.ModelAdmin):
+    """ Item Admin Definition """
+
     pass
 
 
-# @admin.register(RoomType)
-# class RoomTypeAdmin(admin.ModelAdmin):
-#     pass
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    """ Room Admin Definition """
+
+    pass
+
+
+@admin.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    """ Photo Admin Definition """
+    pass
