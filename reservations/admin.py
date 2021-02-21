ab050ed015d6abeef89e9a95ac042ce80858f9cb
bunnycast <berzzubunny@gmail.com>
2021-02-21 22:06:31 +0900

    making boolean functions in_progress and is_finished on reservations App

--- reservations/admin.py
+++ reservations/admin.py
@@ -6,4 +6,14 @@ from reservations.models import Reservation
 @admin.register(Reservation)
 class ReservationAdmin(admin.ModelAdmin):
     """ Reservation Admin Definition """
-    pass
+    list_display = (
+        'room',
+        'status',
+        'check_in',
+        'check_out',
+        'guest',
+        'in_progress',
+        'is_finished',
+    )
+
+    list_filter = ('status',)
