b95c63bed30b5be7b532289fcf06fc33b4bd8e4a
bunnycast <berzzubunny@gmail.com>
2021-02-22 17:42:32 +0900

    making 150 fake rooms by using django-seed

--- rooms/management/commands/seed_rooms.py
+++ rooms/management/commands/seed_rooms.py
@@ -5,7 +5,7 @@ from django.core.management import BaseCommand
 from django_seed import Seed
 
 from users.models import User
-from rooms.models import Room, RoomType, Photo
+from rooms.models import Room, RoomType, Photo, Amenity, Facility, HouseRule
 
 
 class Command(BaseCommand):
@@ -22,23 +22,41 @@ class Command(BaseCommand):
         seeder.add_entity(Room, number, {
             'name': seeder.faker.address(),
             'host': lambda x: random.choice(host_candidate),
-            'room_type': lambda x:random.choice(room_types),
-            'price': lambda x:random.randint(0, 300),
-            'baths': lambda x:random.randint(0, 3),
-            'bedrooms': lambda x:random.randint(0, 5),
-            'beds': lambda x:random.randint(0, 3),
-            'guests': lambda x:random.randint(0, 10),
+            'room_type': lambda x: random.choice(room_types),
+            'price': lambda x: random.randint(0, 300),
+            'baths': lambda x: random.randint(0, 3),
+            'bedrooms': lambda x: random.randint(0, 5),
+            'beds': lambda x: random.randint(0, 3),
+            'guests': lambda x: random.randint(0, 10),
         })
         created_photos = seeder.execute()
         created_clean = flatten(created_photos.values())
 
+        amenities = Amenity.objects.all()
+        facilities = Facility.objects.all()
+        rules = HouseRule.objects.all()
+
         for pk in created_clean:
             room = Room.objects.get(pk=pk)
-            for i in range(3, random.randint(10, 17)):
+            for i in range(3, random.randint(10, 30)):
                 Photo.objects.create(
                     caption=seeder.faker.sentence(),
                     room=room,
                     file=f'room_photos/{random.randint(1, 31)}.webp',
                 )
+            for amenity in amenities:
+                magic_number = random.randint(0, 15)
+                if magic_number % 2 == 0:
+                    room.amenities.add(amenity)
+
+            for facility in facilities:
+                magic_number = random.randint(0, 15)
+                if magic_number % 2 == 0:
+                    room.facilities.add(facility)
+
+            for rule in rules:
+                magic_number = random.randint(0, 15)
+                if magic_number % 2 == 0:
+                    room.house_rules.add(rule)
 
         self.stdout.write(self.style.SUCCESS(f'{number} rooms created!'))
