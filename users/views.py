e38694dacb3b9285884c0aed135977546f2994c0
bunnycast <berzzubunny@gmail.com>
2021-03-03 00:44:49 +0900

    refactoring : LoginView Using FormView

--- users/views.py
+++ users/views.py
@@ -1,12 +1,27 @@
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.views import View
+from django.views.generic import FormView
 
 from users.forms import LoginForm
 
 
-class LoginView(View):
+class LoginView(FormView):
+    template_name = "users/login.html"
+    form_class = LoginForm
+    success_url = reverse_lazy("core:home")
+
+    def form_valid(self, form):
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+            return redirect(reverse("core:home"))
+        return super().form_valid(form)
+
+    """  Refactoring Using FormView 
     def get(self, request):
         form = LoginForm(initial={"email": "abc@de.com"})
         return render(request, "users/login.html", {"form": form})
@@ -22,6 +37,7 @@ class LoginView(View):
                 login(request, user)
                 return redirect(reverse("core:home"))
         return render(request, "users/login.html", {"form": form})
+    """
 
 
 def log_out(request):
