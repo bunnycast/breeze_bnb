79b1c7c67c3f3664cbd19c14a04d2a2252061a09
bunnycast <berzzubunny@gmail.com>
2021-03-03 01:40:11 +0900

    users: SignUpView with FormView

--- users/views.py
+++ users/views.py
@@ -4,7 +4,7 @@ from django.urls import reverse, reverse_lazy
 from django.views import View
 from django.views.generic import FormView
 
-from users.forms import LoginForm
+from users.forms import LoginForm, SignUpForm
 
 
 class LoginView(FormView):
@@ -43,3 +43,25 @@ class LoginView(FormView):
 def log_out(request):
     logout(request)
     return redirect(reverse("core:home"))
+
+
+class SignUpView(FormView):
+    template_name = "users/signup.html"
+    form_class = SignUpForm
+    success_url = reverse_lazy("core:home")
+
+    initial = {
+        "first_name": "Daniel",
+        "last_name": "Kim",
+        "email": "abc@g.com",
+    }
+
+    def form_valid(self, form):
+        form.save()
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+            return redirect(reverse("core:home"))
+        return super().form_valid(form)
