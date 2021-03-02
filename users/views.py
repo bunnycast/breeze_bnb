3175a5902fb0962e68d74c9f8dd5404fb54b618d
bunnycast <berzzubunny@gmail.com>
2021-03-03 00:29:33 +0900

    users : Login and Logout view

--- users/views.py
+++ users/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.views import View
 
 from users.forms import LoginForm
@@ -12,5 +14,16 @@ class LoginView(View):
     def post(self, request):
         form = LoginForm(request.POST)
         if form.is_valid():
-            print(form.cleaned_data)
+            email = form.cleaned_data.get("email")
+            password = form.cleaned_data.get("password")
+            user = authenticate(request, username=email, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect(reverse("core:home"))
         return render(request, "users/login.html", {"form": form})
+
+
+def log_out(request):
+    logout(request)
+    return redirect(reverse("core:home"))
